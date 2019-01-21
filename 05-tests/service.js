d64c7b5515cf91f9241ea91cea8ffa57e44460f3
Galdencio <galdencio@fourup.com.br>
2019-01-21 14:42:07 -0200

    Testes automatizados

---
+++ 05-tests/service.js
@@ -0,0 +1,19 @@
+const { get } = require('axios');
+const URL = `https://swapi.co/api/people`;
+
+async function obterPessoas (nome) {
+    const url = `${URL}/?search=${nome}&format=json`;
+    const response = await get(url);
+    return response.data.results.map(mapearPessoas);
+}
+
+function mapearPessoas (item) {
+    return {
+        nome: item.name,
+        peso: item.mass
+    }
+}
+
+module.exports = {
+    obterPessoas
+}
